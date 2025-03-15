130a648618bf582cfdb8da608407d1403356707c
diegoruiiz64 <diegoruiz64@gmail.com>
2025-03-15 18:57:51 -0500

    feat: implemented hambuerguer menu

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from "react";
 // eslint-disable-next-line no-duplicate-imports
 import type { FC } from "react";
 import { useNavigate } from "@tanstack/react-router";
-
-
 import { Menu, X } from "lucide-react";
 import acuaterraLogo from "../assets/images/logo.png";
 import homeIcon from "../assets/images/home.png";
@@ -19,12 +17,11 @@ import LoaderAcua from "../components/loaders/LoaderAcua";
 const Report: FC = () => {
   const navigate = useNavigate();
 
-  // Control de carga
   const [loading, setLoading] = useState(true);
-  // Control del menú hamburguesa (solo en móviles)
+ 
   const [isOpen, setIsOpen] = useState(false);
 
-  // Simulación de carga
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
@@ -35,7 +32,7 @@ const Report: FC = () => {
     };
   }, []);
 
-  // Cierra el menú si se hace clic fuera (solo en móvil)
+  
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     const handleClickOutside = (event: MouseEvent) => {
@@ -58,7 +55,6 @@ const Report: FC = () => {
     };
   }, [isOpen]);
 
-  // Bloquea scroll vertical cuando el menú está abierto en móvil
   useEffect(() => {
     document.body.style.overflowY = isOpen ? "hidden" : "auto";
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -69,7 +65,7 @@ const Report: FC = () => {
 
   return (
     <div className="flex min-h-screen bg-white font-sans relative overflow-x-auto">
-      {/* Botón de Menú Hamburguesa (solo en móvil) */}
+      
       <button
         className="absolute top-4 left-4 z-50 bg-gray-300 p-2 rounded shadow-md md:hidden"
         id="menu-button"
@@ -78,7 +74,6 @@ const Report: FC = () => {
         {isOpen ? <X size={24} /> : <Menu size={24} />}
       </button>
 
-      {/* Sidebar con transformaciones */}
       <aside
         id="sidebar"
         className={`fixed top-0 left-0 w-64 h-screen bg-gray-300 border-r border-gray-300 flex flex-col transform transition-transform duration-300 ease-in-out z-50
@@ -86,7 +81,7 @@ const Report: FC = () => {
           md:translate-x-0 md:w-64 md:relative`}
       >
         <div className="p-4 flex flex-col items-center relative">
-          {/* Botón de cierre dentro del menú (solo en móvil) */}
+          
           <button
             className="absolute top-2 right-2 p-2 text-gray-700 hover:text-gray-900 md:hidden"
             onClick={() => { setIsOpen(false); }}
@@ -165,8 +160,7 @@ const Report: FC = () => {
           </p>
         </div>
       </aside>
-
-      {/* Contenido principal */}
+      
       <main className="flex-1 p-6 bg-white">
         {loading ? (
           <LoaderAcua />
