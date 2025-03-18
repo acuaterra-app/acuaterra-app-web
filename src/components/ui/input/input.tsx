4f8b8b15d1a3fbdb6f9e98555cc49f0bec923147
diegoruiiz64 <diegoruiz64@gmail.com>
2025-03-18 17:35:56 -0500

    feat: buttom´s styles

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -54,7 +54,7 @@ const InputCustomComponent: FC<InputProps> = ({
           />
         </button>
       )}
-      {error && <span className="error mt-4 text-darkGray font-semibold text-sm mt-1">{error}</span>}
+      {error && <span className="error mt-4 text-darkGray font-semibold text-sm">{error}</span>}
     </div>
   );
 };
