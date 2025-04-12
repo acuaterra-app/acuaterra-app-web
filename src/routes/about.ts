8105da043efde8f3b0709cbedfbd216916ebe038
david <wdperdigon1227@gmail.com>
2025-04-11 22:35:54 -0500

    feat: add authGuard to multiple routes and create vercel.json for rewrites

--- src/routes/about.ts
+++ src/routes/about.ts
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { About } from "../pages/About";
+import { authGuard } from "../common/isTokenValid";
 
 export const Route = createFileRoute("/about")({
 	component: About,
+	beforeLoad: authGuard
 });
--- src/routes/bitacoras.ts
+++ src/routes/bitacoras.ts
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Bitacoras } from '../pages/Bitacoras';
+import { authGuard } from "../common/isTokenValid";
 
 
 export const Route = createFileRoute("/bitacoras")({
     component: Bitacoras,
+    beforeLoad: authGuard
 });
